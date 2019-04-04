5caac8014a31cfc3a7f75de3b4ddd2ec8ed38d28
12ma <mayank.maheshwari625@gmail.com>
2019-04-05 02:55:32 +0530

    second half

--- bin/crypto/cryanalysis/views.py
+++ bin/crypto/cryanalysis/views.py
@@ -3,7 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html',{})
+    import requests
+    import json
+    # Grab crypto price 
+    price_request=requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,BCH,EOS,XLM,ADA,USDT,MIOTA,TRX&tsyms=INR,USD")
+    price=json.loads(price_request.content)
+    # Grab crypto news
+    api_request=requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api=json.loads(api_request.content)
+    return render(request,'home.html',{'api':api, 'price':price})
