417e030d4da3de97588c217a09afb78f546fc5b7
12ma <mayank.maheshwari625@gmail.com>
2019-04-07 16:37:57 +0530

    final

--- bin/crypto/cryanalysis/views.py
+++ bin/crypto/cryanalysis/views.py
@@ -16,3 +16,18 @@ def home(request):
     api_request=requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api=json.loads(api_request.content)
     return render(request,'home.html',{'api':api, 'price':price})
+
+def prices(request):
+    
+    if request.method == 'POST':
+        import requests
+        import json
+  
+        quote = request.POST['quote']
+        quote=quote.upper()
+        crypto_request=requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=INR,USD")
+        crypto=json.loads(crypto_request.content)
+        return render(request, 'prices.html',{'quote':quote,'crypto':crypto})
+    else:
+        notfound = "Enter a crypto-currency symbol into the form above..."
+        return render(request,"prices.html",{'notfound':notfound})    
